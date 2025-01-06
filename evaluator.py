975ba3cb3b028ad1c67cc9f0da5bc895b9fc08ab
erre <ccerre96@gmail.com>
2025-01-06 13:28:42 -0500

    dataloader for mongo db

--- evaluator.py
+++ evaluator.py
@@ -1,3 +1,9 @@
 """Evaluates the results of prompting to Gandalf"""
 import pandas as pd
 import torch
+
+
+
+class AutoGandalfDatabaseLoader:
+    def __init__(self):
+        raise NotImplementedError
