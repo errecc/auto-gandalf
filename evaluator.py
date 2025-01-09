c9de94daf63c130b2606f2c7acd4f7c24706e8ad
erre <ccerre96@gmail.com>
2025-01-09 10:53:14 -0500

    password grabber added to evaluator and removed from utils

--- evaluator.py
+++ evaluator.py
@@ -3,6 +3,17 @@ import pandas as pd
 import torch
 
 
+class PasswordGrabber:
+    def __init__(self, answer):
+        self.answer = answer
+
+    def grab_password(self):
+        """
+        Process an answer from Gandalf and grab the password if it is in the
+        answer, else, return False
+        """
+        raise NotImplementedError
+
 
 class AutoGandalfDatabaseLoader:
     def __init__(self):
